ea692f3f10a7dae6edcd54196c56d2e3ac9ad950
unknown <michaeljunior794@gmail.com>
2024-04-06 09:54:48 +0100

    message

--- CustomerTree.cs
+++ CustomerTree.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Assessed_Exercise_2_Solution
 {
@@ -74,32 +75,41 @@ namespace Assessed_Exercise_2_Solution
             }
         }
 
-        public int Count()
-        {
+        public int Count(){
             return count(root);
         }
 
-        private int count(Node tree)
-        {
+        private int count(Node tree) {
             if (tree == null)
                 return 0;
             return count(tree.left) + count(tree.right) + 1;
         }
 
 
-        private void insertItem(Customer customer, ref Node tree){
-            if (tree == null)
-                tree = new Node(customer);
-
-            else if (customer < tree.Data)
-                insertItem(customer, ref tree.left);
-
-            else if (customer > tree.Data)
-                insertItem(customer, ref tree.right);
+        private void insertItem(Customer customer, ref Node node){
+            if (customer.name.CompareTo(node.Data.name) <= 0){
+                // If the new customer's name is less than or equal to the current node's customer's name, 
+                // go to the left subtree
+                if (node.left == null){
+                    node.left = new Node(customer);
+                } else {
+                    insertItem(customer, ref node.left);    
+                }
+            }else{
+                // If the new customer's name is greater than the current node's customer's name, 
+                // go to the right subtree
+                if (node.right == null){
+                    node.right = new Node(customer);
+                } else{
+                    insertItem( customer, ref node.right);
+                }
+            }
         }
 
-        public void InsertItem(Customer item)
-        { insertItem(item, ref root); }
+        public void InsertItem(Customer item){ 
+            insertItem(item, ref root);
+        }
+
 
 
     }
